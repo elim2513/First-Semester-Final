d3c9127cbf27358f5995e3e754775eefe98df127
elim2513 <8elim@stpatrick.org>
2016-12-12 10:53:15 -0600

    Add files via upload
    Item Update

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s){
+  this.name=n;
+  this.price=p;
+  this.shipping=s;
+}
+
+
+}
+
+//Create your three test items below this comment.
+let mygift = new Item(book,20,3);
+let yourgift = new Item(chair,89,10);
+let bestgift = new Item (basketball,50,5);

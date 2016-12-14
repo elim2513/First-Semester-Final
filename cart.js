93ef59653b42abc2660ed2c1af8bd8d8aee13241
elim2513 <8elim@stpatrick.org>
2016-12-14 11:23:16 -0600

    Add files via upload
    cart update

--- cart.js
+++ cart.js
@@ -5,10 +5,12 @@ class Cart{
   }
 
   addItem(i,q){
-
+    this.itemList.push(i);
+    this.itemQuantity.push(q);
   }
   totalCart(){
     let total=0;
-    for(i=0; i<this.itemlist.length i++)
-  }
-}
+    for(i=0; i<this.itemlist.length i++){
+      total += this.itemList[i].price*this.itemQuantity[]
+      return total; 
+    }

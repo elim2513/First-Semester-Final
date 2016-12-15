2e8e55a969430b06b4cc3a62e819ee1df41db23a
elim2513 <8elim@stpatrick.org>
2016-12-15 10:37:35 -0600

    Add files via upload
    cart finale

--- cart.js
+++ cart.js
@@ -1,16 +1,29 @@
 class Cart{
+  //What is the first part of every class? Type it below.
   constructor(i,q){
     this.itemList=i;
-    this.itemQuantity=q
+    this.itemQuantity=q;
   }
-
+  //Type the instance functions below this comment.
   addItem(i,q){
     this.itemList.push(i);
     this.itemQuantity.push(q);
   }
   totalCart(){
     let total=0;
-    for(i=0; i<this.itemlist.length i++){
-      total += this.itemList[i].price*this.itemQuantity[]
-      return total; 
+    for(i=0; i<=itemList.length; i++){
+      total += this.itemList[i].price*this.itemQuantity[i];
     }
+    return total;
+  }
+  subcart(days){
+    var subCart = new Cart ([],[]);
+    for(let i=0; i<this.itemList.length; i++){
+      if(this.itemList[i].shipping == days){
+        subCart.itemList.push(this.itemList[i]);
+        subCart.itemQuantity.push(this.itemQuantity[i]);
+      }
+    }
+    return subCart;
+  }
+}
